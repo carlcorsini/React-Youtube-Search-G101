38fcdb06dbebfd9b8ae573afd95aa07268fe5bb3
Carl Corsini <carl.m.corsini@gmail.com>
2018-11-27 12:23:31 -0800

    solution

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import YTSearch from 'youtube-api-search'
 import Search from './components/Search'
-
+import Card from './components/Card'
 import './App.scss'
 
 class App extends Component {
@@ -15,9 +15,27 @@ class App extends Component {
   }
 
   render() {
+    let { videos } = this.state
     return (
-      <div className="App">
-        <Search searchYoutube={this.searchYoutube} title="Search on YouTube" />
+      <div className="container">
+        <div className="row">
+          <Search
+            searchYoutube={this.searchYoutube}
+            title="Search on YouTube"
+          />
+        </div>
+        <div className="row">
+          {videos.map(video => (
+            <Card
+              key={video.id.videoId}
+              buttonLink={`https://youtu.be/${video.id.videoId}`}
+              buttonText={'watch this tight ass video dude'}
+              title={video.snippet.title}
+              description={video.snippet.description}
+              videoId={video.id.videoId}
+            />
+          ))}
+        </div>
       </div>
     )
   }
